be0edf22139f1bf0cbc8065bf9c910501be02f52
MichGedd <michaelgeddes555@gmail.com>
2016-09-06 12:14:34 -0400

    Added intake

---
+++ IterativeCode/src/org/usfirst/frc/team1360/robot/teleop/TeleopIntake.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team1360.robot.teleop;
+
+import org.usfirst.frc.team1360.robot.IO.HumanInput;
+import org.usfirst.frc.team1360.robot.IO.RobotOutput;
+
+public class TeleopIntake implements TeleopComponent {
+
+	private static TeleopIntake instance;
+	private RobotOutput robotOutput;
+	private HumanInput humanInput;
+	
+	private TeleopIntake()
+	{
+		this.robotOutput = RobotOutput.getInstance();
+		this.humanInput = HumanInput.getInstance();
+	}
+	
+	public static TeleopIntake getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new TeleopIntake();
+		}
+		
+		return instance;
+	}
+	
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+		robotOutput.intake(humanInput.getOperatorInatke() - humanInput.getOperatorOutake());
+	}
+
+	@Override
+	public void disable() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
