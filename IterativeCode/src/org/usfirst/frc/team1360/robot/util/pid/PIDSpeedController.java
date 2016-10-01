21ce07398e7fd2bb3c17cd7421f1f2a2eb9c260f
jutrasb <jutrasb@gmail.com>
2016-10-01 13:35:09 -0400

    Test changes.

--- IterativeCode/src/org/usfirst/frc/team1360/robot/util/pid/PIDSpeedController.java
+++ IterativeCode/src/org/usfirst/frc/team1360/robot/util/pid/PIDSpeedController.java
@@ -10,5 +10,40 @@ public class PIDSpeedController {
 	private double output;
 	private double setpoint;
 	
+	private double dP;
+	private double dI;
+	private double dD;
+
+	private double dError;
+	
+	private double dInput;
+	private double dOutput;
+	private double dSetpoint;
+
+	public PIDSpeedController(double constantP, double constantI, double constantD)
+	{
+		this.dP = constantP;
+		this.dI = constantI;
+		this.dD = constantD;
+	}
+
+	public void SetConstants(double constantP, double constantI, double constantD)
+	{
+		this.dP = constantP;
+		this.dI = constantI;
+		this.dD = constantD;
+	}
+
+	public void SetP(double constantP) { this.dP = constantP; }
+	public double GetP() { return dP; }
+
+	public void SetI(double constantI) { this.dI = constantI; }
+	public double GetI() { return dI; }
+
+	public void SetD(double constantD) { this.dD = constantD; }
+	public double GetD() { return dD; }
+
+	public double GetError() { return dError; }
+	
 }
  
