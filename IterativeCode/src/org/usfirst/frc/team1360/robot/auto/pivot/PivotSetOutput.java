1829dcc875d794dfe09705791cc62cfd1e361a28
MichGedd <michaelgeddes555@gmail.com>
2016-10-15 15:30:39 -0400

    More PivotAuto

--- IterativeCode/src/org/usfirst/frc/team1360/robot/auto/pivot/PivotSetOutput.java
+++ IterativeCode/src/org/usfirst/frc/team1360/robot/auto/pivot/PivotSetOutput.java
@@ -1,41 +1,5 @@
 package org.usfirst.frc.team1360.robot.auto.pivot;
 
-import org.usfirst.frc.team1360.robot.IO.RobotOutput;
-import org.usfirst.frc.team1360.robot.IO.SensorInput;
-import org.usfirst.frc.team1360.robot.auto.AutonCommand;
-import org.usfirst.frc.team1360.robot.auto.RobotSubsystems;
-import org.usfirst.frc.team1360.robot.util.pid.OrbitPID;
-
-public class PivotSetOutput extends AutonCommand {
-
-	private SensorInput sensroInput;
-	private RobotOutput robotOutput;
-	
-	private double setpoint;
-	private OrbitPID pivotController;
-	
-	public PivotSetOutput(double setpoint) {
-		super(RobotSubsystems.ARM);
-		
-		this.sensroInput = SensorInput.getInstance();
-		this.robotOutput = RobotOutput.getInstance();
-		this.pivotController = new OrbitPID(0.35, 0.001, 0.01, 0.5);
-		
-		this.setpoint = setpoint;
-	}
-
-	@Override
-	public boolean calculate() {
-		this.pivotController.SetSetpoint(setpoint);
-		this.pivotController.CalculateError();
-		this.robotOutput.pivot(this.pivotController.GetOutput());
-		return false;
-	}
-
-	@Override
-	public void override() {
-		// TODO Auto-generated method stub
-		
-	}
+public class PivotSetOutput {
 
 }
